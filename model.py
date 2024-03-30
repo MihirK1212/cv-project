9f0caec8dfca2908c0c061f7964fca49ac800ec4
Mihir Karandikar <mihirkkarandikar@gmail.com>
2024-03-30 11:55:53 +0530

    asdasd

--- model.py
+++ model.py
@@ -88,6 +88,8 @@ class PaCaVIT(torch.nn.Module):
             stage = f'layer_norm_{block_num}'
             layer_norm = getattr(self, stage)
             x = layer_norm(x)
+
+        return {"pred_logits": x}
         
         b, c, h, w = x.size()
         pooler = x.view(b, c * h * w)
