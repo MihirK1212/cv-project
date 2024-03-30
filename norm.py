8e3a627da02b037c5b92616144ac80d203702357
Mihir Karandikar <mihirkkarandikar@gmail.com>
2024-03-30 12:21:12 +0530

    asdasd

--- norm.py
+++ norm.py
@@ -1,5 +1,9 @@
 import torch
 
+import utils
+
+device = utils.get_device()
+
 class LayerNorm2d(torch.nn.Module):
     def __init__(self, features, eps=1e-5):
         super(LayerNorm2d, self).__init__()
@@ -8,6 +12,7 @@ class LayerNorm2d(torch.nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        mean = x.mean(dim=(1, 2, 3), keepdim=True)
-        std = x.std(dim=(1, 2, 3), keepdim=True)
+        x.to(device)
+        mean = x.mean(dim=(1, 2, 3), keepdim=True).to(device)
+        std = x.std(dim=(1, 2, 3), keepdim=True).to(device)
         return self.gamma * (x - mean) / (std + self.eps) + self.beta
